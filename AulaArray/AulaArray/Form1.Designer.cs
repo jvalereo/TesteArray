4330cacc7dca08389b5ca7d28b6364eda8b9cf21
Jonas Valereo <jvalereo2016@gmail.com>
2019-03-28 14:33:59 -0300

    Programa Teste Array
    Projeto, Programa básico em C#, testando o código Array com Vetor

---
+++ AulaArray/AulaArray/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace AulaArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnTeste = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnTeste
+            // 
+            this.BtnTeste.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTeste.Location = new System.Drawing.Point(89, 121);
+            this.BtnTeste.Name = "BtnTeste";
+            this.BtnTeste.Size = new System.Drawing.Size(106, 43);
+            this.BtnTeste.TabIndex = 0;
+            this.BtnTeste.Text = "Teste Vetor";
+            this.BtnTeste.UseVisualStyleBackColor = true;
+            this.BtnTeste.Click += new System.EventHandler(this.BtnTeste_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(62, 31);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(163, 22);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "Testando Arrays";
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSair.Location = new System.Drawing.Point(107, 189);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(73, 33);
+            this.BtnSair.TabIndex = 2;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = true;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Wheat;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ControlBox = false;
+            this.Controls.Add(this.BtnSair);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.BtnTeste);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Teste Arrays";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnTeste;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button BtnSair;
+    }
+}
+
---
+++ AulaArray/AulaArray/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AulaArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTeste_Click(object sender, EventArgs e)
+        {
+            // Aula Teste de Array 
+            // Jonas Valereo Técnico em Informática 
+
+            //Declarando o codigo  do array e  instaciar o vetor
+
+            int [] vetor = new int [4];
+
+            vetor[0] = 7;
+            vetor[1] = 3;
+            vetor[2] = 5;
+            vetor[3] = 1;
+
+            //Declarando a variavel valor
+            int valor = vetor[1];
+
+            System.Array.Sort(vetor);
+
+            //Declarando o codido loop 
+            for (int index = 0; index < 4; index++) 
+            MessageBox.Show(vetor[index].ToString(), "Array");
+
+
+            //Fim do programa 
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            //Declarando o codigo sair
+
+            Close();
+
+        }
+    }
+}
